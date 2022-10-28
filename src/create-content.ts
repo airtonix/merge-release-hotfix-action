34bf47463b464ca0810213af833cca6aedfc17f1
Zeno Jiricek <airtonix@users.noreply.github.com>
2022-10-28 20:10:08 +1030

    add husky and lintstaged

--- src/create-content.ts
+++ src/create-content.ts
@@ -1,3 +1,6 @@
+import kebabCase from 'lodash/fp/kebabCase'
+import template from 'lodash/fp/template'
+
 type CreateContentProps = {
   sourceRef: string
   targetRef: string
@@ -11,25 +14,8 @@ type CreateContentResult = {
   mergeBranchRef: string
 }
 
-export function fromTemplate(template = '', context = {}): string {
-  return Object.entries(context).reduce((result, [key, value]) => {
-    return result.replace(
-      new RegExp(`{{${key}}}`, 'g'),
-      (value ?? '').toString()
-    )
-  }, `${template}`)
-}
-
-export function slugify(text: string, sep = '-'): string {
-  return text
-    .toString()
-    .toLowerCase()
-    .replace(/&/g, ' and ') // Replace & with 'and'
-    .replace(/--+/g, ' ') // Replace multiple - with single -
-    .replace(/[^a-zA-Z0-9_\u3400-\u9FBF\s-]/g, ' ') // Remove all non-word chars
-    .replace(/\s+/g, sep) // Replace spaces with -
-    .replace(new RegExp(`^${sep}+`), '') // Trim - from start of text
-    .replace(new RegExp(`${sep}+$`), '') // Trim - from end of text
+export function slugify(text: string): string {
+  return kebabCase(text.replace(/&/g, '-and-'))
 }
 
 export function createContent({
@@ -45,9 +31,9 @@ export function createContent({
   }
 
   // the merge branch to create
-  const title = fromTemplate(prTitleTemplate, templateContext)
-  const body = fromTemplate(prBodyTemplate, templateContext)
-  const mergeBranchRef = fromTemplate(prBranchTemplate, templateContext)
+  const title = template(prTitleTemplate)(templateContext)
+  const body = template(prBodyTemplate)(templateContext)
+  const mergeBranchRef = template(prBranchTemplate)(templateContext)
 
   return {
     title,
