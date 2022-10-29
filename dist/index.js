71ea1dc23ce6da2d3585a615d6333084d3740757
Zeno Jiricek <airtonix@users.noreply.github.com>
2022-10-29 17:10:57 +1030

    update dist

--- dist/index.js
+++ dist/index.js
@@ -124,7 +124,11 @@ exports.createApi = createApi;
 
 var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
     if (k2 === undefined) k2 = k;
-    Object.defineProperty(o, k2, { enumerable: true, get: function() { return m[k]; } });
+    var desc = Object.getOwnPropertyDescriptor(m, k);
+    if (!desc || ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)) {
+      desc = { enumerable: true, get: function() { return m[k]; } };
+    }
+    Object.defineProperty(o, k2, desc);
 }) : (function(o, m, k, k2) {
     if (k2 === undefined) k2 = k;
     o[k2] = m[k];
@@ -193,7 +197,11 @@ exports.FindBranchPrFactory = FindBranchPrFactory;
 
 var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
     if (k2 === undefined) k2 = k;
-    Object.defineProperty(o, k2, { enumerable: true, get: function() { return m[k]; } });
+    var desc = Object.getOwnPropertyDescriptor(m, k);
+    if (!desc || ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)) {
+      desc = { enumerable: true, get: function() { return m[k]; } };
+    }
+    Object.defineProperty(o, k2, desc);
 }) : (function(o, m, k, k2) {
     if (k2 === undefined) k2 = k;
     o[k2] = m[k];
@@ -380,7 +388,11 @@ exports.Context = Context;
 
 var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
     if (k2 === undefined) k2 = k;
-    Object.defineProperty(o, k2, { enumerable: true, get: function() { return m[k]; } });
+    var desc = Object.getOwnPropertyDescriptor(m, k);
+    if (!desc || ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)) {
+      desc = { enumerable: true, get: function() { return m[k]; } };
+    }
+    Object.defineProperty(o, k2, desc);
 }) : (function(o, m, k, k2) {
     if (k2 === undefined) k2 = k;
     o[k2] = m[k];
@@ -450,6 +462,29 @@ exports.createOrUpdatePr = createOrUpdatePr;
 
 "use strict";
 
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    var desc = Object.getOwnPropertyDescriptor(m, k);
+    if (!desc || ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)) {
+      desc = { enumerable: true, get: function() { return m[k]; } };
+    }
+    Object.defineProperty(o, k2, desc);
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
+    Object.defineProperty(o, "default", { enumerable: true, value: v });
+}) : function(o, v) {
+    o["default"] = v;
+});
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
+    __setModuleDefault(result, mod);
+    return result;
+};
 var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
     function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
     return new (P || (P = Promise))(function (resolve, reject) {
@@ -461,6 +496,7 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
 };
 Object.defineProperty(exports, "__esModule", ({ value: true }));
 exports.getOrCreateBranch = void 0;
+const core = __importStar(__nccwpck_require__(2186));
 const content_1 = __nccwpck_require__(1629);
 function getOrCreateBranch(api, templates, { sourceRef, targetRef }) {
     return __awaiter(this, void 0, void 0, function* () {
@@ -468,13 +504,25 @@ function getOrCreateBranch(api, templates, { sourceRef, targetRef }) {
             source: (0, content_1.slugify)(sourceRef),
             target: (0, content_1.slugify)(targetRef)
         });
+        core.info(`Checking if mergeBranch exists ${mergeBranchRef}`);
+        const potentiallyExistingBranch = yield api.getBranch(mergeBranchRef);
+        if (potentiallyExistingBranch) {
+            core.info(`found mergeBranch ${mergeBranchRef}`);
+            return potentiallyExistingBranch;
+        }
+        core.info(`find sha of sourceRef ${sourceRef}`);
         const sourceBranch = yield api.getBranch(sourceRef);
         if (!sourceBranch)
             throw Error('Source branch not found');
+        core.info(`found sha of sourceRef ${sourceRef}: ${sourceBranch.sha}`);
+        core.info(`create branch of sourceRef ${sourceRef}: ${sourceBranch.sha}`);
         const branch = yield api.createBranch({
             branchName: mergeBranchRef,
             sourceSha: sourceBranch.sha
         });
+        if (!branch)
+            throw new Error(`problem creating new mergeBranch ${mergeBranchRef}`);
+        core.info(`new mergeBranch created ${mergeBranchRef}: ${branch.sha}`);
         return branch;
     });
 }
@@ -490,7 +538,11 @@ exports.getOrCreateBranch = getOrCreateBranch;
 
 var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
     if (k2 === undefined) k2 = k;
-    Object.defineProperty(o, k2, { enumerable: true, get: function() { return m[k]; } });
+    var desc = Object.getOwnPropertyDescriptor(m, k);
+    if (!desc || ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)) {
+      desc = { enumerable: true, get: function() { return m[k]; } };
+    }
+    Object.defineProperty(o, k2, desc);
 }) : (function(o, m, k, k2) {
     if (k2 === undefined) k2 = k;
     o[k2] = m[k];
@@ -551,10 +603,12 @@ function run() {
              */
             const targetRefCollection = Array.from(new Set(targetRefs.split(',').map(branch => branch.trim())));
             for (const targetRef of targetRefCollection) {
+                core.debug(`getOrCreateBranch ${targetRef}`);
                 yield (0, get_or_create_branch_1.getOrCreateBranch)(api, templates, {
                     sourceRef,
                     targetRef
                 });
+                core.debug(`createOrUpdatePr ${targetRef}`);
                 yield (0, create_or_update_pr_1.createOrUpdatePr)(api, templates, {
                     sourceRef,
                     targetRef
